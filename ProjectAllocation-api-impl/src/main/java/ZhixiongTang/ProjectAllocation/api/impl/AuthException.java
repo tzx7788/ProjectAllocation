fd30d046efa28c24e3ba2d507a85ec4e4a14c79b
Tang Zhixiong <tzx7788@me.com>
2014-08-15 10:31:06 +0100

    add AuthException

---
+++ ProjectAllocation-api-impl/src/main/java/ZhixiongTang/ProjectAllocation/api/impl/AuthException.java
@@ -0,0 +1,10 @@
+package ZhixiongTang.ProjectAllocation.api.impl;
+
+public class AuthException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6789519469223081832L;
+
+}
