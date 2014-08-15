815aac39a1bd0270f9677aa1b054a1a7eda141b3
Tang Zhixiong <tzx7788@me.com>
2014-08-15 12:05:53 +0100

    add loadTestData API

--- ProjectAllocation-api-impl/src/main/java/ZhixiongTang/ProjectAllocation/api/impl/DefaultDatabaseService.java
+++ ProjectAllocation-api-impl/src/main/java/ZhixiongTang/ProjectAllocation/api/impl/DefaultDatabaseService.java
@@ -3,6 +3,10 @@ package ZhixiongTang.ProjectAllocation.api.impl;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
 
+import org.ProjectAllocation.model.Professor;
+import org.ProjectAllocation.model.ProfessorPreferenceItem;
+import org.ProjectAllocation.model.Student;
+import org.ProjectAllocation.model.StudentPreferenceItem;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -41,6 +45,34 @@ public class DefaultDatabaseService implements DatabaseService {
 		builder.entity(state.toString());
 		return builder.build();
 	}
+	
+	public Response loadTestData(String authentification, Integer version) {
+		try {
+			authorization(authentification);
+		} catch (AuthException e) {
+			e.printStackTrace();
+			Error error = new Error(e.getMessage());
+			State state = new State(error);
+			ResponseBuilder builder = Response.ok(state);
+			builder.entity(state.toString());
+			return builder.build();
+		}
+		try {
+			if ( version == 1 )
+				this.loadTestData();
+		} catch (DatabaseException e) {
+			e.printStackTrace();
+			Error error = new Error(e.getMessage());
+			State state = new State(error);
+			ResponseBuilder builder = Response.ok(state);
+			builder.entity(state.toString());
+			return builder.build();
+		}
+		State state = new State();
+		ResponseBuilder builder = Response.ok(state);
+		builder.entity(state.toString());
+		return builder.build();
+	}
 
 	private void clear() throws DatabaseException {
 		SessionFactory sf = new Configuration().configure()
@@ -52,6 +84,56 @@ public class DefaultDatabaseService implements DatabaseService {
 			session.createSQLQuery("DELETE FROM Professor").executeUpdate();
 			session.createSQLQuery("DELETE FROM SPreference").executeUpdate();
 			session.createSQLQuery("DELETE FROM PPreference").executeUpdate();
+			tx.commit();
+			session.close();
+		} catch (HibernateException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+
+	private void loadTestData() throws DatabaseException {
+		this.clear();
+		SessionFactory sf = new Configuration().configure()
+				.buildSessionFactory();
+		Session session = sf.openSession();
+		try {
+			Transaction tx = session.beginTransaction();
+			Student s1 = new Student("s1", "tzx1");
+			Student s2 = new Student("s2", "tzx2");
+			Student s3 = new Student("s3", "tzx3");
+			Professor p1 = new Professor("p1", "haha1");
+			Professor p2 = new Professor("p2", "haha1");
+			Professor p3 = new Professor("p3", "haha1");
+			session.save(s1);
+			session.save(s2);
+			session.save(s3);
+			session.save(p1);
+			session.save(p2);
+			session.save(p3);
+			StudentPreferenceItem sr1 = new StudentPreferenceItem(s1, p1, 0);
+			StudentPreferenceItem sr2 = new StudentPreferenceItem(s1, p2, 1);
+			StudentPreferenceItem sr3 = new StudentPreferenceItem(s2, p1, 2);
+			StudentPreferenceItem sr4 = new StudentPreferenceItem(s3, p3, 3);
+			s1.getPreferList().add(sr1);
+			s1.getPreferList().add(sr2);
+			s2.getPreferList().add(sr3);
+			s3.getPreferList().add(sr4);
+			ProfessorPreferenceItem pr1 = new ProfessorPreferenceItem(p1, s1, 0);
+			ProfessorPreferenceItem pr2 = new ProfessorPreferenceItem(p1, s3, 1);
+			ProfessorPreferenceItem pr3 = new ProfessorPreferenceItem(p2, s3, 2);
+			ProfessorPreferenceItem pr4 = new ProfessorPreferenceItem(p3, s2, 3);
+			ProfessorPreferenceItem pr5 = new ProfessorPreferenceItem(p3, s3, 4);
+			p1.getPreferList().add(pr1);
+			p1.getPreferList().add(pr2);
+			p2.getPreferList().add(pr3);
+			p3.getPreferList().add(pr4);
+			p3.getPreferList().add(pr5);
+			session.save(s1);
+			session.save(s2);
+			session.save(s3);
+			session.save(p1);
+			session.save(p2);
+			session.save(p3);
 			tx.commit();
 			session.close();
 		} catch (HibernateException e) {
--- ProjectAllocation-api/src/main/java/ZhixiongTang/ProjectAllocation/api/DatabaseService.java
+++ ProjectAllocation-api/src/main/java/ZhixiongTang/ProjectAllocation/api/DatabaseService.java
@@ -1,17 +1,26 @@
- package ZhixiongTang.ProjectAllocation.api;
+package ZhixiongTang.ProjectAllocation.api;
 
 import javax.ws.rs.DELETE;
 import javax.ws.rs.HeaderParam;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("DatabaseService")
 public interface DatabaseService {
-    @Path( "clear" )
-    @DELETE
-    @Produces( { MediaType.APPLICATION_JSON } )
-    Response clearDatabase( @HeaderParam("authentification") String authentification );
-    
+	@Path("clear")
+	@DELETE
+	@Produces({ MediaType.APPLICATION_JSON })
+	Response clearDatabase(
+			@HeaderParam("authentification") String authentification);
+
+	@Path("loadTestData/{version}")
+	@POST
+	@Produces({ MediaType.APPLICATION_JSON })
+	Response loadTestData(
+			@HeaderParam("authentification") String authentification,
+			@PathParam("version") Integer version);
 }
