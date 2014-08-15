ac0920b4f9caf82f464870e12f27b7f66cae96ae
Tang Zhixiong <tzx7788@me.com>
2014-08-15 11:49:19 +0100

    add student exception

--- ProjectAllocation-api-impl/src/main/java/ZhixiongTang/ProjectAllocation/api/impl/DefaultStudentService.java
+++ ProjectAllocation-api-impl/src/main/java/ZhixiongTang/ProjectAllocation/api/impl/DefaultStudentService.java
@@ -7,6 +7,7 @@ import javax.ws.rs.core.Response.ResponseBuilder;
 
 import org.ProjectAllocation.model.Professor;
 import org.ProjectAllocation.model.Student;
+import org.hibernate.HibernateException;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -20,53 +21,31 @@ public class DefaultStudentService implements
 		ZhixiongTang.ProjectAllocation.api.StudentService {
 
 	public Response getInformationFromSID(String sid) {
-		SessionFactory sf = new Configuration().configure()
-				.buildSessionFactory();
-		Session session = sf.openSession();
-		Transaction tx = session.beginTransaction();
-		String hql = "from Student where SID=:sid";
-		Query query = session.createQuery(hql);
-		query.setString("sid", sid);
-		@SuppressWarnings("unchecked")
-		List<Student> list = query.list();
-		tx.commit();
-		session.close();
-		if (list.size() == 0) {
-			Error error = new Error("No student found!");
-			State state = new State(error);
-			ResponseBuilder builder = Response.ok(state);
-			builder.entity(state.toString());
-			return builder.build();
-		} else {
-			Student s = list.get(0);
+		try {
+			Student s = this.findStudent(sid);
 			State state = new State(s);
 			ResponseBuilder builder = Response.ok(state);
 			builder.entity(state.toString());
 			return builder.build();
-		}
-	}
-
-	public Response getPreferenceListFromSID(String sid) {
-		SessionFactory sf = new Configuration().configure()
-				.buildSessionFactory();
-		Session session = sf.openSession();
-		Transaction tx = session.beginTransaction();
-		String hql = "from Student where SID=:sid";
-		Query query = session.createQuery(hql);
-		query.setString("sid", sid);
-		if (query.list().size() == 0) {
-			tx.commit();
-			session.close();
-			Error error = new Error("No student found!");
+		} catch (StudentException e) {
+			Error error = new Error(e.getMessage());
 			State state = new State(error);
 			ResponseBuilder builder = Response.ok(state);
 			builder.entity(state.toString());
 			return builder.build();
-		} else {
-			Student s = (Student) query.list().get(0);
-			List<Professor> list = s.preferProfessorsList();
-			tx.commit();
-			session.close();
+		} catch (DatabaseException e) {
+			Error error = new Error(e.getMessage());
+			State state = new State(error);
+			ResponseBuilder builder = Response.ok(state);
+			builder.entity(state.toString());
+			return builder.build();
+		}
+
+	}
+
+	public Response getPreferenceListFromSID(String sid) {
+		try {
+			List<Professor> list = this.findPreferProfessorsList(sid);
 			JSONArray result = new JSONArray();
 			for (Professor professor : list) {
 				result.put(professor.toJSONObject());
@@ -75,6 +54,63 @@ public class DefaultStudentService implements
 			ResponseBuilder builder = Response.ok(state);
 			builder.entity(state.toString());
 			return builder.build();
+		} catch (StudentException e) {
+			Error error = new Error(e.getMessage());
+			State state = new State(error);
+			ResponseBuilder builder = Response.ok(state);
+			builder.entity(state.toString());
+			return builder.build();
+		} catch (DatabaseException e) {
+			Error error = new Error(e.getMessage());
+			State state = new State(error);
+			ResponseBuilder builder = Response.ok(state);
+			builder.entity(state.toString());
+			return builder.build();
+		}
+	}
+
+	private Student findStudent(String sid) throws StudentException,
+			DatabaseException {
+		SessionFactory sf = new Configuration().configure()
+				.buildSessionFactory();
+		Session session = sf.openSession();
+		try {
+			Transaction tx = session.beginTransaction();
+			String hql = "from Student where SID=:sid";
+			Query query = session.createQuery(hql);
+			query.setString("sid", sid);
+			@SuppressWarnings("unchecked")
+			List<Student> list = query.list();
+			tx.commit();
+			session.close();
+			if (list.size() == 0)
+				throw new StudentException("No student found!");
+			return list.get(0);
+		} catch (HibernateException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+
+	private List<Professor> findPreferProfessorsList(String sid)
+			throws StudentException, DatabaseException {
+		SessionFactory sf = new Configuration().configure()
+				.buildSessionFactory();
+		Session session = sf.openSession();
+		try {
+			Transaction tx = session.beginTransaction();
+			String hql = "from Student where SID=:sid";
+			Query query = session.createQuery(hql);
+			query.setString("sid", sid);
+			@SuppressWarnings("unchecked")
+			List<Student> list = query.list();
+			if (list.size() == 0)
+				throw new StudentException("No student found!");
+			List<Professor> result = list.get(0).preferProfessorsList();
+			tx.commit();
+			session.close();
+			return result;
+		} catch (HibernateException e) {
+			throw new DatabaseException(e.getMessage());
 		}
 	}
 }
---
+++ ProjectAllocation-api-impl/src/main/java/ZhixiongTang/ProjectAllocation/api/impl/StudentException.java
@@ -0,0 +1,13 @@
+package ZhixiongTang.ProjectAllocation.api.impl;
+
+public class StudentException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1433004978923025885L;
+	
+	public StudentException(String description){
+		super(description);
+	}
+}
