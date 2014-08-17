ce76863a542f98664f41d806fa36ea2d8e822934
Tang Zhixiong <tzx7788@me.com>
2014-08-17 23:55:33 +0100

    add AdminException

---
+++ ProjectAllocation-api/src/main/java/ZhixiongTang/ProjectAllocation/api/exception/AdminException.java
@@ -0,0 +1,13 @@
+package ZhixiongTang.ProjectAllocation.api.exception;
+
+public class AdminException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3917316501883729793L;
+
+	public AdminException(String description) {
+		super(description);
+	}
+}
